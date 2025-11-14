6a57b1c32642bac1263820c8f04ed7bb1b41f1fb
GamalielMS92 <gamalielms92@gmail.com>
2025-11-14 13:00:27 +0100

    fix warning

---
+++ app/success.tsx
@@ -0,0 +1,10 @@
+// app/success.tsx
+import { Text, View } from 'react-native';
+
+export default function SuccessScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Email verificado correctamente</Text>
+    </View>
+  );
+}
